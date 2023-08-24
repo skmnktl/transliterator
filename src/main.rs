d0f7d7722a5da5138bbdd1d3bed0508de8ee4e1a
śrīnivāsa kaśyap munukutla <skmnktl@gmail.com>
2023-08-23 23:15:12 -0400

    rebuild

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+/*
+"sarasvatI" -> [s,a,r,a,s,v,a,t,I]
+
+*/
+fn main() {
+    println!("Hello, world!");
+}
---
+++ src/types.rs
@@ -0,0 +1,72 @@
+pub enum Vowels{
+    a,
+    A,
+    i,
+    I,
+    u,
+    U,
+    RRi,
+    RRI,
+    LLi,
+    LLI,
+    ^e,
+    e
+    E,
+    ai,
+    ^o,
+    o,
+    O,
+    au
+}
+
+pub enum Yogavahas{
+    M,
+    H,
+    .N
+}
+
+pub enum Symbols{
+    .h,
+    cbindu
+}
+
+pub enum Svaras{
+    anudaatta,
+    udaatta,
+    svarita
+}
+
+pub enum Consonants {
+    k,
+    kh,
+    g,
+    gh,
+    c,
+    ch,
+    j,
+    jh,
+    h,
+    t,
+    th,
+    d,
+    dh,
+    n,
+    p,
+    ph,
+    b,
+    bh,
+    m,
+    y,
+    r,
+    l,
+    v,
+    s,
+    h,
+    L, 
+    k,
+    j,
+}
+
+pub struct Matrka{
+    value: 
+}
