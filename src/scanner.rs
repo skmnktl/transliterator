c99a66a8a0ab3c1107375e33b89bf6596ce56391
śrīnivāsa kaśyap munukutla <skmnktl@gmail.com>
2023-08-25 18:23:43 -0400

    adding scanner -- for indic and roman both

--- src/scanner.rs
+++ src/scanner.rs
@@ -4,37 +4,39 @@
  */
 use unicode_segmentation::UnicodeSegmentation;
 
-pub fn indic_to_chars() -> Vec<char>{
-    todo!()
-}
-
-pub fn roman_to_chars(input: &String) -> Vec<char>{
-    let result = input.chars().collect();
+pub fn indic_to_chars(input: String) -> Vec<String>{
+    let val = input.as_str();
+    let result  = UnicodeSegmentation::graphemes(input.as_str(), true).into_iter().collect::<Vec<&str>>();
+    let result: Vec<String> = result.iter().map(|&s|s.into()).collect();
     result
+    
 }
 
 
+pub fn roman_to_chars(input: String) -> Vec<String>{
+    let result = input.chars().map(|c| c.to_string()).collect::<Vec<String>>();
+    result.to_vec()
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    
     #[test]
     fn test_roman_to_chars() {
         let input: String = "mahāsarasvatī".to_string();
-        let expected = vec!['m','a','h','ā','s','a','r','a','s','v','a','t','ī'];
-        println!("{:?}", roman_to_chars(&input));
-        assert_eq!(roman_to_chars(&input), expected);
+        let expected = vec!["m","a","h","ā","s","a","r","a","s","v","a","t","ī"].into_iter().map(|c| c.to_string()).collect::<Vec<String>>();
+        assert_eq!(roman_to_chars(input), expected);
    
     }
     
     #[test]
     fn test_indic_to_chars() {
         let input: String = "महासरस्वती".to_string();
-        let expected = vec!['म', 'ह', 'ा', 'स','र','स', '्','व', 'त', 'ी'];
-        println!("{:?}", roman_to_chars(&input));
-        assert_eq!(roman_to_chars(&input), expected);
+        let expected = vec!["म", "हा", "स", "र", "स्", "व", "ती"].into_iter().map(|c| c.to_string()).collect::<Vec<String>>();
+        assert_eq!(indic_to_chars(input), expected);
    
     }
+
 
 }
