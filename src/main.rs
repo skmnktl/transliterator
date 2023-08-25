2e283d19ae372c9564072430e194ce810910b181
śrīnivāsa kaśyap munukutla <skmnktl@gmail.com>
2023-08-25 08:24:08 -0400

    adding Token trait to all the token subtypes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,38 @@
 /*
-"sarasvatI" -> [s,a,r,a,s,v,a,t,I]
+"sarasvatI" -> [Consonant::s,VowelMarks::a,r,a,s,v,a,t,I]
 
 */
-fn main() {
-    println!("Hello, world!");
+
+
+use std::collections::HashMap;
+use toml;
+
+mod types;
+
+
+#[derive(Debug, Clone)]
+pub enum ScriptName {
+    Devanagari,
 }
+
+type ScriptMap = HashMap<String, HashMap<String, toml::Value>>;
+
+pub fn read_script(which: &ScriptName) -> ScriptMap {
+    let script = match *which {
+        ScriptName::Devanagari => include_str!("../maps/deva.toml"),
+    };
+    toml::from_str(script).unwrap()
+}
+
+fn main() {
+    let script = ScriptName::Devanagari;
+    println!("{:?}",read_script(&script));
+}
+
+
+mod test{
+    //SLP1: aSman -> a, S, m, a, n
+    //ITRANS: ashman -> a, sh, m, a, n
+
+}
+ 
--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,23 @@
-pub enum Vowels{
+
+/* 
+  What is a token? 
+    A token is either a Vowel, VowelMark, Consonant, Yogavaha, Symbol, or Svara.
+    To check if a given type is a Token, we need to implement an is_token method. 
+    is_token always returns true when it is implemented. But having the trait lets use
+    do some very nice type checking. 
+
+    Later on, we can also consider implementing token addition and sandhi rules within the train.
+*/
+pub trait Token {
+       
+    fn is_token(&self) -> bool{
+        true
+    }
+
+} 
+
+#[allow(non_camel_case_types)]
+pub enum Vowel {
     a,
     A,
     i,
@@ -9,43 +28,81 @@ pub enum Vowels{
     RRI,
     LLi,
     LLI,
-    ^e,
-    e
+    _e,
+    e,
     E,
     ai,
-    ^o,
+    _o,
     o,
     O,
-    au
+    R, // tamil ra or bandira in telugu
+    au,
 }
 
-pub enum Yogavahas{
+#[allow(non_camel_case_types)]
+pub enum VowelMark {
+    a,
+    A,
+    i,
+    I,
+    u,
+    U,
+    RRi,
+    RRI,
+    LLi,
+    LLI,
+    _e,
+    e,
+    E,
+    ai,
+    _o,
+    o,
+    O,
+    R, // tamil ra or bandira in telugu
+    au,
+}
+
+
+#[allow(non_camel_case_types)]
+pub enum Yogavaha {
     M,
     H,
-    .N
+    N,
 }
 
-pub enum Symbols{
-    .h,
-    cbindu
+
+#[allow(non_camel_case_types)]
+pub enum Symbol {
+    h,
+    cbindu,
 }
 
-pub enum Svaras{
+
+#[allow(non_camel_case_types)]
+pub enum Svara {
     anudaatta,
     udaatta,
-    svarita
+    svarita,
 }
 
-pub enum Consonants {
+
+#[allow(non_camel_case_types)]
+pub enum Consonant {
     k,
     kh,
     g,
     gh,
+    _N,
     c,
     ch,
     j,
     jh,
-    h,
+    _n,
+    T,
+    Th,
+    D,
+    Dh,
+    N,
     t,
     th,
     d,
@@ -60,13 +117,49 @@ pub enum Consonants {
     r,
     l,
     v,
-    s,
+    sh,
+    Sh,
+    sa,
     h,
-    L, 
-    k,
-    j,
+    L,
 }
 
-pub struct Matrka{
-    value: 
+
+impl Token for Vowel{}
+
+impl Token for VowelMark{}
+
+impl Token for Consonant{}
+
+impl Token for Yogavaha{}
+
+impl Token for Svara{}
+
+impl Token for Symbol{}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_token_impl() {
+        // verify that the Token trait is implemented for each of the token types. 
+        let x = Vowel::a;
+        assert_eq!(x.is_token(), true);
+
+        let x = VowelMark::a;
+        assert_eq!(x.is_token(), true);
+
+        let x = Consonant::k;
+        assert_eq!(x.is_token(), true);
+
+        let x = Yogavaha::H;
+        assert_eq!(x.is_token(), true);
+
+        let x = Symbol::h;
+        assert_eq!(x.is_token(), true);
+
+        let x = Svara::anudaatta;
+        assert_eq!(x.is_token(), true);
+    }
 }
